bdfa03ac40e2930a1b3a541b2e882a8790466ce6
github username <github email address>
2016-09-26 18:52:32 -0400

    Done.

--- nyc_pigeon_organizer3.rb
+++ nyc_pigeon_organizer3.rb
@@ -1,5 +1,4 @@
-require "pry"
-
+require 'pry' 
 pigeon_data = {
   :color => {
     :purple => ["Theo", "Peter Jr.", "Lucky"],
@@ -20,18 +19,27 @@ pigeon_data = {
 }
 
 def nyc_pigeon_organizer(data)
-  # write your code here!
-  pigeon_data = Hash.new
-  data.each do |category, trait_list|
-    trait_list.each do |trait, pigeon_list|
-      pigeon_list.each do |pigeon|
-        if !pigeon_data.keys.include?(pigeon) then pigeon_data[pigeon] = {} end
-        if !pigeon_data[pigeon].keys.include?(category) then pigeon_data[pigeon][category] = [] end
-        pigeon_data[pigeon][category] << trait.to_s
-      end
+
+data.each_with_object({}) do |(key, h), result|
+#takes the hash and operates on the top level key and each value
+
+            binding.pry 
+ h.each do |value, names|
+    
+    #collects the top level keys and looks for names (values)
+    names.each do |name|
+      #takes the names from all the values 
+      result[name] ||= {} 
+
+      #and makes them into a new hash
+      result[name][key] ||= []
+      #and adds arrays in which to put the values
+      result[name][key] << value.to_s
+      #and fetches the values and puts them in those arrays 
     end
   end
-  pigeon_data
+end 
+
 end
 
 nyc_pigeon_organizer(pigeon_data)
